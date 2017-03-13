2096cdc6c3aa134159a83706325d3ff5d7d5daa9
esemin83 <esemin83@gmail.com>
2017-03-13 23:50:17 +0300

    23_2 Двухуровневая архитектура

--- application.py
+++ application.py
@@ -15,6 +15,8 @@ from TestStack.White.UIItems.Finders import *
 clr.AddReferenceByName("UIAutomationTypes, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35")
 from System.Windows.Automation import *
 
+from fixture.group import GroupHelper
+
 
 class App:
 
@@ -22,32 +24,4 @@ class App:
         self.application = Application.Launch("C:\\addressbook_exe\\AddressBook.exe")
         self.main_window = self.application.GetWindow("Free Address Book")
 
-    def get_group_list(self):
-        main_window = self.main_window
-        modal = self.open_group_editor()
-        tree = modal.Get(SearchCriteria.ByAutomationId("uxAddressTreeView"))
-        root = tree.Nodes[0]
-        l = [node.Text for node in root.Nodes]
-        self.close_group_editor(main_window)
-        return l
-
-    def add_new_group(self, name):
-        main_window = self.main_window
-        modal = self.open_group_editor()
-        modal.Get(SearchCriteria.ByAutomationId("uxNewAddressButton")).Click()
-        modal.Get(SearchCriteria.ByControlType(ControlType.Edit)).Enter(name)
-        Keyboard.Instance.PressSpecialKey(KeyboardInput.SpecialKeys.RETURN)
-        self.close_group_editor(main_window)
-
-    def close_group_editor(self, main_window):
-        main_window.Get(SearchCriteria.ByAutomationId("uxCloseAddressButton")).Click()
-
-    def open_group_editor(self):
-        main_window = self.main_window
-        main_window.Get(SearchCriteria.ByAutomationId("groupButton")).Click()
-        modal = main_window.ModalWindow("Group editor")
-        return modal
-
-    def exit(self):
-        main_window = self.main_window
-        main_window.Get(SearchCriteria.ByAutomationId("uxExitAddressButton")).Click()
+        self.group = GroupHelper(self)
